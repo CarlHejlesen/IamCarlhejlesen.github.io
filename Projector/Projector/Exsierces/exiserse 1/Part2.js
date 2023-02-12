7df0030dbfd0ed90050dcc505a674dfe2adab370
CarlHejlesen <113053807+CarlHejlesen@users.noreply.github.com>
2023-02-12 12:32:02 +0100

    Done with part 2

--- Projector/Projector/Exsierces/exiserse 1/Part2.js
+++ Projector/Projector/Exsierces/exiserse 1/Part2.js
@@ -29,42 +29,47 @@ msgBoard.printMessages();//Runs printMessages function
 
 //register eventHandlers (call-back functions)
 function briansHandler(boardName,message){//function declaration takes in boardName"IWP Chat" and a message
-    console.log(`Brian! A message from ${boardName}: ${message}`);
+    console.log(`Brian! A message from ${boardName}: ${message} line 32`); //This prints
 }
-msgBoard.register(briansHandler);
-msgBoard.register((board,message)=>console.log(`Board ${board} says to Michele: ${message}`));
-msgBoard.sendAndNotifyMessage("URGENT: Opgaveregning nu!")
+msgBoard.register(briansHandler);//This is a function call, with a function as parameter input. register will save this function in an array
+msgBoard.register((board,message)=>console.log(`Board ${board} says to Michele: ${message} line 35`)); // this also sends a function
+msgBoard.sendAndNotifyMessage("URGENT: Opgaveregning nu! line 36")// This sends string to function the function.
+//The function will save the message in messages array and run the function[0] in handlers with the input parameters board and message.
+// There is curently 2 functions in the handlers array, BriansHandler & function 35.
 
-/* EXERCISE, brug gerne et constructor funktion til at oprette msgBoards */
+/* EXERCISE, brug gerne et constructor funktion til at oprette msgBoards
+Same Exercise just solved in a diffrent way.
+ */
 
-function MessageBoard(boardname){
-    this.handlers= [];
-    this.messages= [];
+function MessageBoard(boardname){//function declaration
+    this.handlers= [];//This is how you declare an array that is bound to this indivual function call
+    this.messages= [];// Array declaration for list local function call
     this.boardName=boardname;
-    this.register= function(f){
-        this.handlers.push(f);
+    this.register= function(f){//Declaring an function local to a function call
+        this.handlers.push(f);//Saves parameter in handlers array
     };
-    this.printMessages =function(){
+    this.printMessages =function(){//Prints all strings in messages array
         console.log("Messages History in board "+ this.boardName+":");
-        for(let m of this.messages)
+        for(let m of this.messages) // As many elements in this array for loop will run
             console.log(m);
     };
-    this.putMessage= function (m){
+    this.putMessage= function (m){//puts m (string) in messages array
         this.messages.push(m);
     };
-    this.sendAndNotifyMessage= function(message){
+    this.sendAndNotifyMessage= function(message){//will save message and run functions saved in handlers, with 2 strings as parameter input
         this.messages.push(message); //better: this.putMessage(message);
-        for(let f of this.handlers)
-            f(this.boardName,message);
+        for(let f of this.handlers)// as many elements in handlers array the loop will run
+            f(this.boardName,message);//runs saved function with the locas boardname, and the input string/message
     }
 }
-let board2= new MessageBoard("Opgave Regning");
+let board2= new MessageBoard("Opgave Regning");// Here make an reforence board2 = function with this input parameter
 
-board2.putMessage("Hej, dette er en test");
-board2.printMessages();
+board2.putMessage("Hej, dette er en test");// Saves string to locas messages array in board2
+board2.printMessages(); // prints all saved strings in local array in function board2
 
 //register eventHandler, get message, call all back
 
-board2.register(briansHandler);
-board2.register((board,message)=>console.log(`Board ${board} says to Michele: ${message}`));
-board2.sendAndNotifyMessage("URGENT: Opgaveregning nu!: line 70")
+board2.register(briansHandler); //Saves function briansHandler in handlers array that locas for the board2 function
+board2.register((board,message)=>console.log(`Board ${board} says to Michele: ${message}`)); // Also saves function in local function
+board2.sendAndNotifyMessage("URGENT: Opgaveregning nu!: line 70");//This runs all the functions saved in the local array handler with the locas input parameters
+
--- Projector/Projector/Exsierces/exiserse 1/exserserises quiestions.md
+++ Projector/Projector/Exsierces/exiserse 1/exserserises quiestions.md
@@ -2,33 +2,33 @@
 In this exercise we will gradually use more and more aspects of JS functions on a simple example.
 let diceRoll=[1,6,6,2,3,4,6];
 
-## 1)
+### 1)
 Write a function get6s_v1 that takes an array of numbers (dice values 1..6) as parameter, and prints the indexes and corresponding values of the array where elements are 6s. E.g., 1:6, 2:6, 6:6 ..." excluding other elements. At this stage, just write a basic version, eg. without using functions as parameters
 Apply the function to the above array as argument.
 
-## 2)
+### 2)
 Write a helper function eg "is6(v)" that can test if the supplied parameter value v is a 6
 Rewrite get6s_v1 to get6_v2 such that it uses the helper function to test if the index/value should be printed.
 Apply the function to the above array.
 
-## 3)
+### 3)
 Now create a get6s_v3 that rewrites get6_v2 to accept a dice array as first parameter, and a "compare" function as second parameter.
 Apply the function using the diceRoll and function "is6" as actual arguments
 
-## 4)
+### 4)
 Copy and rename the get6s_v3 function to findDices_v1(dice,compare). Call it with the same arguments as above using the diceRoll and function "is6"
 Then call it using a function expression (named or anonymous) at the call site, to pass a function that selects 1 dices.
 
-## 5)
+### 5)
 Then call it using a lambda expression to pass a function that selects dices with values <= 3.
 
-## 6)
+### 6)
 (advanced:)  what does the following statement produce?
 diceRoll.filter(dice=>dice==6).reduce((sum,dice)=>sum+dice,0);
 
 # Part 2. Objects & Arrays
 
-## 1)
+### 1)
 Declare an object "msgBoard" that represents a message board that a set of clients can use to exchange messages.
 It should have a property for the  name of the message board, initialized to "IWP Chat", and another property (array) for storing the history of messages.
 Add a function (method) to the object called "putMessage" that takes a string message as argument at stores it in the message array.
@@ -43,7 +43,7 @@ Messages History in board IWP Chat:
 Hej, dette er en test
 Hej IWP
 
-## 2)
+### 2)
 Add a function "register(f)" to the object that clients use to be notified when new messages arrives (a so-called call-back function): The register function takes a function f as argument, and stores the passed function in a different array "callBacks". f itself takes two parameters: the name of the message board, and the message. Then prints them to the console, possibly in an client specific manner (eg language).
 Based on the putMessage method, add a similar one "sendAndNotify" that in addition to storing the message (eg using putMessage) also calls all registered call-back functions whenever a message arrives (supplying the registered function with the new message, and boardname.
 
@@ -59,9 +59,10 @@ It should result in the following output
 Brian! A message from IWP Chat: URGENT: Opgaveregning nu!
 Board IWP Chat says to Michele: URGENT: Opgaveregning nu!
 
-## 3)
+### 3)
 Make a constructor function  MessageBoard(name) that makes message boards.
 Create a few message boards.
 
  let board2= new MessageBoard("Opgave Regning");
  board2.putMessage("Hej, dette er en test");
+ 
